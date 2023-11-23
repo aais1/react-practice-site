f7ed301717ee6117e85ebf5e180b85fb4b396506
AliAais <aaisali228@gmail.com>
2023-11-23 01:20:39 -0800

    adding more stuff

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,24 @@
+
+function Analytics() {
+    return (
+        <div className='bg-white  p-14'>
+            <div className="flex flex-col w-[80vw] mx-auto gap-32 items-center md:flex-row">
+                <img src="src/assets/laptop.jpg" alt="Laptop" className="md:w-[350px] w-[430px]"/>
+                <div className=" ">
+                    <p className='text-blue-500 text-4xl my-4 font-bold'>Learn From Experts</p>
+                    <p className='font-medium'>Explore the fascinating world of knowledge and skill acquisition.
+                    Discover the art of learning from experienced professionals who are passionate about
+                    sharing their expertise. Whether it's gaining insights, mastering new techniques,
+                    or delving into advanced concepts, our expert-led sessions provide a rich learning experience.
+                    Join us on a journey of discovery and enhance your understanding with the guidance of seasoned
+                    mentors.</p>
+                    <div className="text-center md:text-start">
+                        <button className='border-2 border-black hover:bg-black hover:text-white w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black del duration-200'>Discover More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,19 @@
+
+const Newsletter = () => {
+  return (
+    <div className="bg-blue-900 p-8 ">
+        <div className=" text-white flex flex-col w-[80vw] mx-auto  items-center gap-10 md:flex-row md:py-20 md:gap-32  ">
+            <div className="">
+                <h1 className="text-2xl font-bold text-blue-500 underline underline-offset-4">Want To Learn</h1>
+                <p className="text-xl">Subscribe to our newsletter for the latest updates.</p>
+            </div>
+            <div className="flex flex-col gap-2 md:flex-row justify-center">
+                <input className="h-12 w-96 p-2 rounded-tl rounded-bl text-black " type="text" placeholder="Enter your email address"/>
+                <button className="bg-red-600 text-white rounded-tr rounded-br px-3 py-2 hover:bg-red-800">Subscribe</button>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Newsletter;
